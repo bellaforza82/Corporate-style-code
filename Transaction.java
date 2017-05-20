22104f8ac2b10ba2d366895944e019d048e79bd4
bellaforza82 <neilallenjones@gmail.com>
2017-05-20 11:48:23 -0400

    Add files via upload

---
+++ Transaction.java
@@ -0,0 +1,39 @@
+import java.util.Comparator;
+import java.util.Date;
+
+/**
+ * Created by horry on 5/20/2017.
+ */
+public class Transaction {
+    private final String who;
+    private final Date when;
+    private final double amount;
+
+    public Transaction(String who, Date when, double amount){
+        this.who = who;
+        this.when = when;
+        this.amount = amount;
+    }
+
+    public static class WhoOrder implements Comparator<Transaction>{
+        public int  compare(Transaction v, Transaction w){
+            return v.who.compareTo(w.who);
+        }
+    }
+    public static class WhenOrder implements Comparator<Transaction>{
+        public int  compare(Transaction v, Transaction w){
+            return v.when.compareTo(w.when);
+        }
+    }
+    public static class HowMuchOrder implements Comparator<Transaction>{
+        public int  compare(Transaction v, Transaction w){
+            if (v.amount < w.amount) return -1;
+            if (v.amount > w.amount) return  1;
+            return 0;
+        }
+    }
+
+
+
+
+}
